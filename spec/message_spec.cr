adb31c275eb3c98bb4ba72f1122c85c16033e808
Joakim Reinert <mail@jreinert.com>
2015-09-25 13:42:32 +0200

    use test class in message spec for better isolation

--- spec/message_spec.cr
+++ spec/message_spec.cr
@@ -1,11 +1,21 @@
 require "./spec_helper"
 
 module DDP
+  class TestMessage < Message
+    json_mapping({
+      foo: String
+      bar: String
+    })
+  end
+
   describe Message do
     describe ".parse" do
       it "returns an instance of the message type passed in the json" do
-        msg = Message.parse(%({"msg": "connect", "version": "1"}))
-        msg.should be_a(Connect)
+        msg = Message.parse(%({"msg": "test_message", "foo": "bar", "bar": "baz"}))
+        msg.should be_a(TestMessage)
+        raise "foo" unless msg.is_a?(TestMessage)
+        msg.foo.should eq("bar")
+        msg.bar.should eq("baz")
       end
     end
   end
